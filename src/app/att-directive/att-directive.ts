87bb7cce5ec8e2120ba005ffb87da49d592a4166
Gopal <gopal.kuppireddy@tagnos.com>
2025-12-26 19:15:06 +0530

    component life cycle and pipe directive changes

--- src/app/att-directive/att-directive.ts
+++ src/app/att-directive/att-directive.ts
@@ -1,14 +1,48 @@
-import { NgClass, NgStyle } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { JsonPipe, NgClass, NgStyle } from '@angular/common';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, OnDestroy, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-att-directive',
-  imports: [NgClass,NgStyle],
+  imports: [NgClass,NgStyle,JsonPipe],
   templateUrl: './att-directive.html',
   styleUrl: './att-directive.css',
 })
-export class AttDirective {
+export class AttDirective implements OnInit,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy {
 div1ClassName=signal<string>("");
+
+constructor(){
+  console.log("constructor called");
+}
+
+ngOnInit(): void {
+  console.log("ngOnInit called");
+  //totrigger api calls
+  // to subscribe 
+}
+ngAfterContentInit(): void {
+  console.log("ngAfterContentInit called");
+  
+}
+ngAfterContentChecked(): void {
+  console.log("ngAfterContentChecked called");
+}
+ngAfterViewInit(): void {
+  console.log("ngAfterViewInit called");
+  // to view child initilaization
+}
+ngAfterViewChecked(): void {
+  console.log("ngAfterViewChecked called");
+}
+ngOnDestroy(): void {
+  console.log("ngOnDestroy called");
+  // to unsubscribe or clean up code before component is destroyed
+}
+
+student : object = {
+  name : 'Gopal Reddy',
+  age  : 33,
+  address : 'Hyderabad'
+}
 
 div2Color : boolean = false;
 
